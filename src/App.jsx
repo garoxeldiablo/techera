cb42f5d263775bd9254f89c067be18803108b092
jocjoc <89018472+garoxeldiablo@users.noreply.github.com>
2024-05-13 16:39:39 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Login } from "./components/login.jsx";
+import  Navbar  from "./components/navbarclient.jsx";
+import Clientarea from "./components/clientarea.jsx";
+// import { Login } from "./components/login.jsx";
 
 export default function App(){
     return(
         <>
-        <Login/>
+        <Navbar/>
+        <Clientarea/>
         </>
     )
 }
