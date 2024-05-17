00f9d5b9b9b75e8f306bb38af9f71df14c3a06b9
gideonap <gideonaprakoso@gmail.com>
2024-05-17 14:15:29 +0700

    Update navbarclient.jsx

--- src/components/navbarclient.jsx
+++ src/components/navbarclient.jsx
@@ -81,7 +81,7 @@ export default function NavbarClient(){
                                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                 alt=""
                             />
-                            <p className='text-base text-blue-900 mx-2 font-semibold'>Agus</p>
+                            <p className='text-base text-blue-900 mx-2 font-semibold'>Agus Bisana</p>
                             </Menu.Button>
                         </div>
                         <Transition
@@ -97,7 +97,7 @@ export default function NavbarClient(){
                             <Menu.Item>
                                 {({ active }) => (
                                 <a
-                                    href="#"
+                                    href="/editprofile"
                                     className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-blue-900')}
                                 >
                                     Your Profile
