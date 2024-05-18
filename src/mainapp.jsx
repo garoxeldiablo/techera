3741a76b0bfd38e3e076917fc0a35c4e353971ab
Riyanridha <riyanridha26@gmail.com>
2024-05-18 14:27:48 +0700

    Update mainapp.jsx

--- src/mainapp.jsx
+++ src/mainapp.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Routes ,Route, useLocation } from "react-router-dom";
 // import { SignIn } from "./components/signin.jsx";
 // import { SignUp } from "./components/signup.jsx";
@@ -7,23 +8,32 @@ import { Routes ,Route, useLocation } from "react-router-dom";
 // import Forum from "./components/forum.jsx";
 // import Blog from "./components/blog.jsx";
 import BookAlert from "./components/bookalert.jsx";
+=======
+import { Routes ,Route } from "react-router-dom";
+import { SignIn } from "./components/signin.jsx";
+import { SignUp } from "./components/signup.jsx";
+import Home from "./components/home.jsx";
+import ClientArea from "./components/clientarea.jsx";
+import EditProfile from "./components/editprofile.jsx";
+import Forum from "./components/forum.jsx";
+import Blog from "./components/blog.jsx";
+// import BookAlert from "./components/riyan.jsx";
+>>>>>>> f422e5588ed788f62642c08eeefaf6172a192e64
 
 
 export default function MainApp(){
-    const location = useLocation()
     return(
         <>
-        {/* <Routes>
+        <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
-        </Routes> */}
-        
-        {/* <EditProfile/> */}
-
-        {/* <Forum/> */}
-        {/* <Blog/> */}
-        <BookAlert/>
+            <Route path="/clientarea" element={<ClientArea />}/>
+            <Route path="/editprofile" element={<EditProfile />}/>
+            <Route path="/forum" element={<Forum />}/>
+            <Route path="/blog" element={<Blog />}/>
+        </Routes>
+        {/* <BookAlert/> */}
         </>
         
     )
