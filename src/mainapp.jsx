95b4e32c8a3f211b408cbaebfc25d29f15577a09
Riyanridha <riyanridha26@gmail.com>
2024-05-19 01:13:24 +0700

    Update mainapp.jsx

--- src/mainapp.jsx
+++ src/mainapp.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { Routes ,Route, useLocation } from "react-router-dom";
-// import { SignIn } from "./components/signin.jsx";
-// import { SignUp } from "./components/signup.jsx";
-// import NavbarClient from "./components/navbarclient.jsx";
-// import Home from "./components/home.jsx";
-// import EditProfile from "./components/editprofile.jsx";
-// import Forum from "./components/forum.jsx";
-// import Blog from "./components/blog.jsx";
-import BookAlert from "./components/bookalert.jsx";
-=======
 import { Routes ,Route } from "react-router-dom";
 import { SignIn } from "./components/signin.jsx";
 import { SignUp } from "./components/signup.jsx";
@@ -17,9 +6,13 @@ import ClientArea from "./components/clientarea.jsx";
 import EditProfile from "./components/editprofile.jsx";
 import Forum from "./components/forum.jsx";
 import Blog from "./components/blog.jsx";
+<<<<<<< HEAD
 // import BookAlert from "./components/bookalert.jsx";
 >>>>>>> f422e5588ed788f62642c08eeefaf6172a192e64
 
+=======
+import BookAlert from "./components/bookalert.jsx";
+>>>>>>> 6ca5bf808ebca31302859dc4805962bceae9cff2
 
 export default function MainApp(){
     return(
@@ -31,7 +24,9 @@ export default function MainApp(){
             <Route path="/clientarea" element={<ClientArea />}/>
             <Route path="/editprofile" element={<EditProfile />}/>
             <Route path="/forum" element={<Forum />}/>
+            <Route path="/createpost" element={<CreatePost />}/>
             <Route path="/blog" element={<Blog />}/>
+            <Route path="/bookalert" element={<BookAlert />}/>
         </Routes>
         {/* <BookAlert/> */}
         </>
