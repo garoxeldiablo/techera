39eab45c27fc512dd504fc61110ca6e8aaa321d6
jocjoc <89018472+garoxeldiablo@users.noreply.github.com>
2024-05-18 20:02:45 +0700

    fixed bookalert

--- src/mainapp.jsx
+++ src/mainapp.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { Routes ,Route, useLocation } from "react-router-dom";
-// import { SignIn } from "./components/signin.jsx";
-// import { SignUp } from "./components/signup.jsx";
-// import NavbarClient from "./components/navbarclient.jsx";
-// import Home from "./components/home.jsx";
-// import EditProfile from "./components/editprofile.jsx";
-// import Forum from "./components/forum.jsx";
-// import Blog from "./components/blog.jsx";
-import BookAlert from "./components/bookalert.jsx";
-=======
 import { Routes ,Route } from "react-router-dom";
 import { SignIn } from "./components/signin.jsx";
 import { SignUp } from "./components/signup.jsx";
@@ -17,9 +6,7 @@ import ClientArea from "./components/clientarea.jsx";
 import EditProfile from "./components/editprofile.jsx";
 import Forum from "./components/forum.jsx";
 import Blog from "./components/blog.jsx";
-// import BookAlert from "./components/riyan.jsx";
->>>>>>> f422e5588ed788f62642c08eeefaf6172a192e64
-
+import BookAlert from "./components/bookalert.jsx";
 
 export default function MainApp(){
     return(
@@ -32,6 +19,7 @@ export default function MainApp(){
             <Route path="/editprofile" element={<EditProfile />}/>
             <Route path="/forum" element={<Forum />}/>
             <Route path="/blog" element={<Blog />}/>
+            <Route path="/bookalert" element={<BookAlert />}/>
         </Routes>
         {/* <BookAlert/> */}
         </>
