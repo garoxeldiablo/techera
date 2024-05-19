4f98f1bd7bcf1e92ece6c173583407381942ae7c
Riyanridha <riyanridha26@gmail.com>
2024-05-19 20:34:36 +0700

    Create subscribe.jsx

---
+++ src/components/subscribe.jsx
@@ -0,0 +1,29 @@
+export default function Subscribe(){
+    return(
+        <>
+        <div className="w-[622.19px] h-[546.33px] pl-[31.94px] pr-[28.14px] pt-[30.91px] pb-[29.33px] bg-neutral-100 rounded-[8.99px] shadow border-2 border-blue-800 flex-col justify-center items-end gap-[93.07px] inline-flex">
+    <div className="w-[562.11px] h-[356.01px] relative flex-col justify-start items-start flex">
+        <div className="w-[294.48px] text-black text-[43.37px] font-bold font-['Roboto'] leading-[58.73px]">Join our newsletter!</div>
+        <div className="w-[562.11px] text-black text-lg font-normal font-['Roboto'] leading-[27.11px]">Unlock a world of exclusive content, timely updates, and special offers by subscribing to our newsletter. Be the first to receive the latest insights, exciting news, and personalized recommendations tailored just for you!</div>
+        <div className="h-[85px] flex-col justify-center items-center gap-1 inline-flex">
+            <div className="self-stretch h-[85px] flex-col justify-start items-start gap-2 flex">
+                <div className="self-stretch justify-start items-start gap-2.5 inline-flex">
+                    <div className="text-neutral-600 text-base font-normal font-['Roboto'] leading-[25px]">Email</div>
+                </div>
+                <div className="self-stretch h-[52px] px-4 py-2 rounded-lg border border-neutral-600 flex-col justify-start items-start gap-2.5 flex">
+                    <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start flex">
+                        <div className="self-stretch text-neutral-600 text-base font-normal font-['Roboto'] leading-[25px]">Enter your email</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="shadow justify-start items-start inline-flex">
+        <div className="w-[111px] h-[37px] px-[45px] py-2.5 bg-blue-800 rounded-lg justify-center items-center flex">
+            <div className="text-white text-base font-semibold font-['Roboto'] leading-normal">Send</div>
+        </div>
+    </div>
+</div>
+        </>
+    )
+}
